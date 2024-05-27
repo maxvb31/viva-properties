300ce9b1d25422e634dbbc281b5100fb609ff622
maxvb31 <109318178+maxvb31@users.noreply.github.com>
2024-05-27 02:20:09 +0100

    fixed slider 4

--- frontend/src/pages/Properties.js
+++ frontend/src/pages/Properties.js
@@ -67,40 +67,42 @@ export default function Properties() {
       )
     : posts;
 
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 8,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 5,
-          initialSlide: 3,
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 8,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 5,
+            initialSlide: 0,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5, // Adjusted to 5
+            initialSlide: 0,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3, // Adjusted to 3
+            initialSlide: 0,
+          }
         }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 4,
-          initialSlide: 3,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 3,
-          initialSlide: 3,
-        }
-      }
-    ]
-  };
+      ]
+    };
+    
+    
 
   return (
     <section>
